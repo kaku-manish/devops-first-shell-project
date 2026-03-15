acb18a6e4f466bff089abc565c5a715beca60672
kaku-manish <kakumanish2004@gmail.com>
2026-03-15 19:37:40 +0530

    updated calculator script

--- calculator.sh
+++ calculator.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+echo "simple calculator"
+echo "Enter first number:"
+read a
+
+echo "Enter second number"
+read b
+
+echo "Choose operation (+, -):"
+read op
+
+case $op in
+	"+") echo "Result: $((a+b))" ;;
+	"-") echo "Result: $((a-b))" ;;
+esac
